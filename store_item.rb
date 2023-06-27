a0c9394930f0ddd2b2fb72ff6e31491fe3d503cb
Yassar Ahmed <yassarahmed17@gmail.com>
2023-06-27 14:27:51 -0500

    adds customer purchase

--- store_item.rb
+++ store_item.rb
@@ -1,3 +1,6 @@
 item1 = { :item_type => "Hat", :hat_team => "Chicago Bulls", :price => 12 }
 item2 = { :item_type => "Hat", :hat_team => "Miami Heat", :price => 13 }
 item3 = { :item_type => "Hat", :hat_team => "Boston Celctics", :price => 11 }
+
+puts "customer purchased a #{item1[:item_type]}, it was a  
+#{item1[:hat_team]} hat. Customer paid #{item1[:price]} dollars"
