e13d7f25b13343a75bba3ca3849f505fb19d1f39
Yassar Ahmed <yassarahmed17@gmail.com>
2023-06-27 14:21:57 -0500

    adds three items to store

---
+++ store_item.rb
@@ -0,0 +1,3 @@
+item1 = { :item_type => "Hat", :hat_team => "Chicago Bulls", :price => 12 }
+item2 = { :item_type => "Hat", :hat_team => "Miami Heat", :price => 13 }
+item3 = { :item_type => "Hat", :hat_team => "Boston Celctics", :price => 11 }
