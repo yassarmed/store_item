4ccb18af0ed6f8420b23c725c4e4a8c58684fdf5
Yassar Ahmed <yassarahmed17@gmail.com>
2023-06-28 13:09:14 -0500

    uses input_options hash instead of array

--- store_item.rb
+++ store_item.rb
@@ -42,39 +42,23 @@
 # item1.print_purchase
 
 class Storeitem
-  attr_reader :item_type, :price
+  attr_reader :item_type, :price, :hat_team
   attr_writer :hat_team
 
-  def initialize(input_item_type, input_hat_team, input_price)
-    @item_type = input_item_type
-    @hat_team = input_hat_team
-    @price = input_price
+  def initialize(input_options)
+    @item_type = input_options[:item_type]
+    @hat_team = input_options[:hat_team]
+    @price = input_options[:price]
   end
-
-  # def item_type
-  #   @item_type
-  # end
-
-  # def hat_team
-  #   @hat_team
-  # end
-
-  # def price
-  #   @price
-  # end
-
-  # def hat_team=(input_hat_team)
-  #   @hat_team = input_hat_team
-  # end
 
   def print_purchase
     puts "Customer purchased a #{@item_type}, it was #{@hat_team} merchandise, customer paid #{@price} dollars"
   end
 end
 
-item1 = Storeitem.new("Hat", "Chicago Bulls", 12)
-item2 = Storeitem.new("Hat", "Miami Heat", 13)
-item3 = Storeitem.new("Hat", "Boston Celtics", 11)
+item1 = Storeitem.new({ item_type: "Hat", hat_team: "Chicago Bulls", price: 12 })
+item2 = Storeitem.new({ item_type: "Hat", hat_team: "Miami Heat", price: 13 })
+item3 = Storeitem.new({ item_type: "Hat", hat_team: "Boston Celtics", price: 11 })
 
 item1.print_purchase
 item2.print_purchase
